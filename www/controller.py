abd18958cc11d74e6db8551a1a10e7b5aa8c9651
Dan Baumfeld <dbaumfeld@flowroute.com>
2014-12-03 10:18:15 -0800

    stubbed out all pages, and continued build of main nav

--- www/controller.py
+++ www/controller.py
@@ -17,40 +17,79 @@ def home():
 @main.route('/voice')
 def voice():
     """ voice page """
-    return render_template("pages/voice.html")
+    return render_template(
+        "pages/voice.html",
+        section=".voice", has_subpage=False
+    )
 
 
 @main.route('/solutions')
 def solutions():
     """ solutions page """
-    return render_template("pages/solutions.html")
+    return render_template(
+        "pages/solutions.html",
+        section=".solutions", has_subpage=True
+    )
 
 
 @main.route('/solutions/unified-communications')
 def unified_communication():
     """ unified communications page """
-    return render_template("pages/unified-communications.html")
+    return render_template(
+        "pages/unified-communications.html",
+        section=".solutions", has_subpage=True
+    )
+
+
+@main.route('/solutions/call-center')
+def call_center():
+    """ call center page """
+    return render_template(
+        "pages/call-center.html",
+        section=".solutions", has_subpage=True
+    )
+
+
+@main.route('/solutions/pbx-and-phone-systems')
+def pbx_and_phone_systems():
+    """ pbx and phone systems page """
+    return render_template(
+        "pages/pbx-and-phone-systems.html",
+        section=".solutions", has_subpage=True
+    )
 
 
 @main.route('/pricing')
 def pricing():
     """ pricing page """
-    return render_template("pages/pricing.html")
+    return render_template(
+        "pages/pricing.html",
+        section=".pricing", has_subpage=False
+    )
 
 
 @main.route('/developers')
 def developers():
     """ developers page """
-    return render_template("pages/developers.html")
+    return render_template(
+        "pages/developers.html",
+        section=".developers", has_subpage=False
+    )
 
 
 @main.route('/partners')
 def partners():
     """ partners page """
-    return render_template("pages/partners.html")
+    return render_template(
+        "pages/partners.html",
+        section=".partners", has_subpage=False
+    )
 
 
 @main.route('/login')
 def login():
     """ login page """
-    return render_template("pages/login.html")
+    return render_template(
+        "pages/login.html",
+        section=".login", has_subpage=False
+    )
